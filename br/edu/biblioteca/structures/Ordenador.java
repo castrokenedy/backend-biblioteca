6bdc29f29d4c8306b0054cff12f3368f5c32b144
Gabriel Oliveira Ramos <50968825+gabrielramos-06@users.noreply.github.com>
2026-05-05 14:05:33 -0300

    Ordenador.java

--- br/edu/biblioteca/structures/Ordenador.java
+++ br/edu/biblioteca/structures/Ordenador.java
@@ -1,11 +1,23 @@
 package br.edu.biblioteca.structures;
+
 import br.edu.biblioteca.model.Livro;
 
 public class Ordenador {
+    public static void ordenarLivrosPorTitulo(Vetor<Livro> livros) {
+        for (int i = 0; i < livros.size() - 1; i++) {
+            for (int j = 0; j < livros.size() - 1 - i; j++) {
+                if (livros.get(j).getTitulo().compareToIgnoreCase(livros.get(j + 1).getTitulo()) > 0) {
+                    Livro temp = livros.get(j);
+                    livros.set(j, livros.get(j + 1));
+                    livros.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
     public static void ordenarLivrosPorAno(Vetor<Livro> livros) {
-        int n = livros.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) 
+        for (int i = 0; i < livros.size() - 1; i++) {
+            for (int j = 0; j < livros.size() - 1 - i; j++) {
                 if (livros.get(j).getAno() > livros.get(j + 1).getAno()) {
                     Livro temp = livros.get(j);
                     livros.set(j, livros.get(j + 1));
