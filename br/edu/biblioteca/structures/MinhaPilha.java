342d4b7e5e59d39d569c3d3e79c790232fdd053b
Gabriel Oliveira Ramos <50968825+gabrielramos-06@users.noreply.github.com>
2026-05-05 13:38:34 -0300

    MinhaPilha.java

--- br/edu/biblioteca/structures/MinhaPilha.java
+++ br/edu/biblioteca/structures/MinhaPilha.java
@@ -1,33 +1,26 @@
 package br.edu.biblioteca.structures;
 
-public class MinhaPilha<T> {
-    private class No {
-        T dado;
-        No proximo;
-        No(T dado) { this.dado = dado; }
-    }
-
-    private No topo;
+public class MinhaPilha<T> 
+    private Vetor<T> vetor = new Vetor<>();
 
     public void push(T elemento) {
-        No novoNo = new No(elemento);
-        novoNo.proximo = topo;
-        topo = novoNo;
+        vetor.add(elemento);
     }
 
     public T pop() {
         if (isEmpty()) return null;
-        T dado = topo.dado;
-        topo = topo.proximo;
-        return dado;
+        int ultimaPosicao = vetor.size() - 1;
+        T elemento = vetor.get(ultimaPosicao);
+        vetor.remove(ultimaPosicao);
+        return elemento;
     }
 
     public T peek() {
         if (isEmpty()) return null;
-        return topo.dado;
+        return vetor.get(vetor.size() - 1);
     }
 
     public boolean isEmpty() {
-        return topo == null;
+        return vetor.size() == 0;
     }
 }
